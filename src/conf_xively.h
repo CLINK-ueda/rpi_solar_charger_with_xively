9989362512e90a2652e63ff2d917de368e86e2db
Yasuhiro ISHII <ishii.yasuhiro@gmail.com>
2013-06-08 15:35:56 +0900

    added Makefile and something

---
+++ src/conf_xively.h
@@ -0,0 +1,17 @@
+/*
+
+  Solar Monitor for RaspberryPi
+
+  Coded by Yasuhiro ISHII,2013
+
+*/
+
+
+#ifdef __CONF_XIVELY_H__
+#define __CONF_XIVELY_H__
+
+#define XIVELY_API_KEY		"SET_YOUR_OWN_API_KEY"
+#define XIVELY_FEED_ID		"SET_YOUR_OWN_FEED_ID"
+#define XIVELY_DATASTREAM_ID	"SET_YOUR_OWN_DATASTREAM_ID"
+
+#endif /* __CONF_XIVELY_H__ */
